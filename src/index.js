c5dd02f4f23a6450d1a8a78a9ba9dfb205865e31
Lore Wait For It <macbookpro@MC.local>
2020-02-06 16:01:34 -0600

    Pequeño cambio en index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
 console.log("Hola Mundo");
 
-function suma(num1, num2) {
+function suma(num1,num2) {
   return num1 + num2;
 }
 
 var resultado = suma(2, 3);
 
-console.log('La suma de los dos números es: ', suma(2, 3));
+console.log('La suma de los dos números es: ', suma(2,3));
 console.log('La suma de los dos números es: ', resultado);
